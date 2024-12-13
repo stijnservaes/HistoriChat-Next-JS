0290c3adf21b1aae1af3c71f67bea5f034380446
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-12-13 17:19:01 +0100

    feat: Handle Select Room

--- app/chatroom/components/SelectForm.tsx
+++ app/chatroom/components/SelectForm.tsx
@@ -1,37 +1,27 @@
 "use client";
 import {
-  Alert,
   Box,
   Button,
   FormControl,
-  IconButton,
   InputLabel,
   MenuItem,
   Select,
   Typography,
 } from "@mui/material";
-import Collapse from "@mui/material/Collapse";
-import CloseIcon from "@mui/icons-material/Close";
-import CircularProgress from "@mui/material/CircularProgress";
-import Form from "next/form";
-import { selectRoom } from "../actions";
 import MeetingRoomIcon from "@mui/icons-material/MeetingRoom";
 import { Room } from "../page";
-import { useActionState, useEffect, useState } from "react";
+import { useState } from "react";
+import { redirect } from "next/navigation";
 
 export default function SelectForm({ roomList }: { roomList: Room[] }) {
-  const [state, formAction, isPending] = useActionState(selectRoom, {
-    error: false,
-    message: "",
-  });
-  const [errorVisible, setErrorVisible] = useState(true);
+  const [selectedRoom, setSelectedRoom] = useState(roomList[0].id)
 
-  useEffect(() => setErrorVisible(true), [state]);
+  function handleClick() {
+    redirect(`/chatroom/${selectedRoom}`)
+  }
 
   return (
     <Box
-      component={Form}
-      action={formAction}
       sx={{
         display: "flex",
         flexDirection: "column",
@@ -41,59 +31,32 @@ export default function SelectForm({ roomList }: { roomList: Room[] }) {
       }}
     >
       <Typography variant="h5">Choose your Chatroom</Typography>
-      {state.error && errorVisible && (
-        <Box sx={{ position: "absolute", top: -80, width: 250 }}>
-          <Collapse in={errorVisible}>
-            <Alert
-              severity="error"
-              variant="outlined"
-              action={
-                <IconButton
-                  aria-label="close"
-                  size="small"
-                  color="inherit"
-                  onClick={() => setErrorVisible(false)}
-                >
-                  <CloseIcon fontSize="inherit" />
-                </IconButton>
-              }
-            >
-              {state.message}
-            </Alert>
-          </Collapse>
-        </Box>
-      )}
-      {isPending ? (
-        <Box sx={{display: "flex", justifyContent: "center"}}>
-          <CircularProgress />
-        </Box>
-      ) : (
-        <>
-          <FormControl fullWidth>
-            <InputLabel id="room-select">Chatroom</InputLabel>
-            <Select
-              labelId="room-select"
-              name="selectedRoom"
-              label="Chatroom"
-              defaultValue={1}
-            >
-              {roomList.map((room) => (
-                <MenuItem key={room.id} value={room.id}>
-                  {room.name}
-                </MenuItem>
-              ))}
-            </Select>
-          </FormControl>
-          <Button
-            endIcon={<MeetingRoomIcon />}
-            variant="contained"
-            type="submit"
-            color="success"
-          >
-            Enter the Time Machine
-          </Button>
-        </>
-      )}
+
+      <FormControl fullWidth>
+        <InputLabel id="room-select">Chatroom</InputLabel>
+        <Select
+          labelId="room-select"
+          name="selectedRoom"
+          label="Chatroom"
+          value={selectedRoom}
+          onChange={(e) => setSelectedRoom(Number(e.target.value))}
+        >
+          {roomList.map((room) => (
+            <MenuItem key={room.id} value={room.id}>
+              {room.name}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+      <Button
+        endIcon={<MeetingRoomIcon />}
+        variant="contained"
+        type="submit"
+        color="success"
+        onClick={handleClick}
+      >
+        Enter the Time Machine
+      </Button>
     </Box>
   );
 }
