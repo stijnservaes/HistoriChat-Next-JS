709ac7bcaed8e63a29a2b8f6916a335a5faac118
Stijn Servaes <142609421+ComradeStijn@users.noreply.github.com>
2024-12-13 21:40:46 +0100

    feat: Return to choose chatroom page button

--- app/chatroom/[roomId]/components/ChatWindow.tsx
+++ app/chatroom/[roomId]/components/ChatWindow.tsx
@@ -14,6 +14,7 @@ import {
 } from "@mui/material";
 import CircularProgress from "@mui/material/CircularProgress";
 import SendIcon from "@mui/icons-material/Send";
+import NoMeetingRoomIcon from "@mui/icons-material/NoMeetingRoom";
 import { useEffect, useRef, useState } from "react";
 import { io, Socket } from "socket.io-client";
 import { redirect } from "next/navigation";
@@ -31,12 +32,13 @@ export default function ChatWindow({
   const [socket, setSocket] = useState(null as Socket | null);
   const [error, setError] = useState({ success: true, message: "test" });
   const [isLoading, setIsLoading] = useState(false);
+  const [isSending, setIsSending] = useState(false);
   const [exceedToken, setExceedToken] = useState({
     exceeds: false,
     message: "test",
   });
   const [inputMessage, setInputMessage] = useState("");
-  const chatWindow = useRef<HTMLDivElement | null>(null)
+  const chatWindow = useRef<HTMLDivElement | null>(null);
 
   const { getToken } = useAuth();
   const { user } = useUser();
@@ -45,8 +47,15 @@ export default function ChatWindow({
     if (!socket) return;
 
     if (inputMessage) {
+      setIsSending(true);
       socket.emit("chat", { roomId: roomId, message: inputMessage });
       setInputMessage("");
+    }
+  }
+
+  function handleKeyDown(e: React.KeyboardEvent<HTMLInputElement>) {
+    if (e.key === "Enter") {
+      handleSend();
     }
   }
 
@@ -55,7 +64,6 @@ export default function ChatWindow({
       chatWindow.current.scrollTop = chatWindow.current.scrollHeight;
     }
   }, [messages, isLoading]);
-
 
   useEffect(() => {
     let newSocket: Socket | null = null;
@@ -103,10 +111,12 @@ export default function ChatWindow({
 
     socket.on("chat", (payload: { success: boolean; message: ChatMessage }) => {
       setMessages((prev) => [...prev, payload.message]);
+      setIsSending(false);
     });
 
     socket.on("no_tokens", (msg: string) => {
       setExceedToken({ exceeds: true, message: msg });
+      setIsSending(false);
     });
 
     return () => {
@@ -123,7 +133,18 @@ export default function ChatWindow({
     <Stack direction="column" gap={2} sx={{ height: "100%", padding: 2 }}>
       {exceedToken.exceeds && (
         <Fade in={true}>
-          <Alert variant="standard" severity="warning" sx={{position: "fixed", top: "20vh", left: "50vw", transform: "translateX(-50%)"}}>{exceedToken.message}</Alert>
+          <Alert
+            variant="standard"
+            severity="warning"
+            sx={{
+              position: "fixed",
+              top: "20vh",
+              left: "50vw",
+              transform: "translateX(-50%)",
+            }}
+          >
+            {exceedToken.message}
+          </Alert>
         </Fade>
       )}
       {!error.success ? (
@@ -161,9 +182,14 @@ export default function ChatWindow({
       ) : (
         <>
           <Paper
-          ref={chatWindow}
+            ref={chatWindow}
             elevation={1}
-            sx={{ flexGrow: 1, padding: 3, overflowY: "auto", scrollBehavior: "smooth" }}
+            sx={{
+              flexGrow: 1,
+              padding: 3,
+              overflowY: "auto",
+              scrollBehavior: "smooth",
+            }}
           >
             {messages.map((message) => (
               <ChatBubble
@@ -174,26 +200,48 @@ export default function ChatWindow({
             ))}
           </Paper>
           <Stack direction="row">
-            <TextField
-              value={inputMessage}
-              onChange={(e) => setInputMessage(e.target.value)}
-              id="new-message"
-              label="Send message"
-              variant="outlined"
-              sx={{ flexGrow: 1 }}
-            />
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "center",
-                alignItems: "center",
-                marginLeft: 2,
-              }}
-            >
-              <IconButton onClick={handleSend}>
-                <SendIcon />
-              </IconButton>
-            </Box>
+            {isSending ? (
+              <Box sx={{ marginX: "auto", marginTop: 1 }}>
+                <CircularProgress />
+              </Box>
+            ) : (
+              <>
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: -2,
+                    marginRight: 1
+                  }}
+                >
+                  <IconButton onClick={() => redirect("/chatroom")}>
+                    <NoMeetingRoomIcon />
+                  </IconButton>
+                </Box>
+                <TextField
+                  value={inputMessage}
+                  onChange={(e) => setInputMessage(e.target.value)}
+                  onKeyDown={handleKeyDown}
+                  id="new-message"
+                  label="Send message"
+                  variant="outlined"
+                  sx={{ flexGrow: 1 }}
+                />
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "center",
+                    alignItems: "center",
+                    marginLeft: 2,
+                  }}
+                >
+                  <IconButton onClick={handleSend}>
+                    <SendIcon />
+                  </IconButton>
+                </Box>
+              </>
+            )}
           </Stack>
         </>
       )}
